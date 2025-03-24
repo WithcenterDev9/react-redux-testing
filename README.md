a74a7030d1c87859bd058ef3e3883daa9a363100
WithcenterDev6 <withcenter.dev9@gmail.com>
2025-03-24 16:55:21 +0800

    added redux

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-This is my self bootstrapped react app. It contains:
-- React 
-- Playwright (for E2E testing)
-- TailwindCSS
-- Vite
-- Typescript
+# React-Redux
+
+
+## Technologies that I used
+-   Tailwindcss
+-   Vite
+-   Github Pages
+-   ReactJS
+-   Redux
+
+
+## Notes
+When creating redux. There step that you must follow.
+1. Install `@reduxjs/toolkit` and `react-redux` via `npm`
+2. You must create a `store` first, and later `slice`
+    -   a. configureStore: `src/state/store.ts`
+    -   b. createSlice: `src/state/count.slice.ts`
+    -   c. export your slice actions, reducers
+    -   d. provide store
+    -   e. consume it with `useSelector` and `dispatch` function
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import CountComponent from "./components/count_component";
+
 export default function App() {
     return (
-        <div className="bg-blue-500 text-white p-4">
-            <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
-            <p>This is a simple React app styled with Tailwind CSS.</p>
-        </div>
+        <>
+            <section className="bg-blue-500 text-white p-4 mb-8">
+                <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+                <p>This is a simple React app styled with Tailwind CSS.</p>
+            </section>
+            <section className="flex items-center justify-center">
+                <CountComponent />
+            </section>
+        </>
+
+
     );
 }
